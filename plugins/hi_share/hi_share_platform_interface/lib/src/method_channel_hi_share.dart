c5fd8f8cf0d18a42f4dbf2b192a839e42083ea6b
YangJianxiang <tospery@gmail.com>
2022-11-23 00:56:46 +0800

    hi_share_platform_interface

--- plugins/hi_share/hi_share_platform_interface/lib/src/method_channel_hi_share.dart
+++ plugins/hi_share/hi_share_platform_interface/lib/src/method_channel_hi_share.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-
 import 'hi_share_platform.dart';
 
-/// An implementation of [HiSharePlatform] that uses method channels.
 class MethodChannelHiShare extends HiSharePlatform {
-  /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('hi_share');
 
   @override
-  Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
-    return version;
+  Future<bool> show() async {
+    final result = await methodChannel.invokeMethod<bool>('show');
+    return result ?? false;
   }
 }
