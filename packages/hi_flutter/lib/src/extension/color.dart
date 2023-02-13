37537ceecaa2e4c4ad05466c17603572c36c3f3e
tospery <tospery@gmail.com>
2023-02-13 18:34:57 +0800

    color

--- packages/hi_flutter/lib/src/extension/color.dart
+++ packages/hi_flutter/lib/src/extension/color.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 extension ColorFlutterEx on Color {
-  Color get dividerColor => computeLuminance() < 0.5 ? Colors.red : Colors.green;
+  Color get hintColor => computeLuminance() < 0.5 ? Colors.grey : Colors.grey;
+  Color get dividerColor => computeLuminance() < 0.5 ? Colors.grey.shade300 : Colors.grey.shade300;
+  Color get scaffoldBackgroundColor => computeLuminance() < 0.5 ? Colors.grey.shade50 : Colors.grey.shade50;
 }
