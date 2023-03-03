eb39ded9c518dcf025822472924dbcee901d06d8
tospery <tospery@gmail.com>
2023-03-03 15:29:06 +0800

    api

--- packages/hi_flutter/lib/src/extension/context.dart
+++ packages/hi_flutter/lib/src/extension/context.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:hi_flutter/src/core/function.dart' as hi_flutter;
 
 extension BuildContextEx on BuildContext {
+  EdgeInsets get safeArea => MediaQuery.of(this).padding;
   double metricWidth(double width) => hi_flutter.metricWidth(width, context: this);
   double metricHeight(double height) => hi_flutter.metricHeight(height, context: this);
 
