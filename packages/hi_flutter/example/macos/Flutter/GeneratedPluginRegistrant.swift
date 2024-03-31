29bd92b565fbbfbb0412abea6100bd800d6038f0
tospery <tospery@gmail.com>
2024-04-01 07:09:57 +0800

    hi

--- packages/hi_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/hi_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,19 +5,23 @@
 import FlutterMacOS
 import Foundation
 
+import appkit_ui_element_colors
 import device_info_plus
 import dynamic_color
 import macos_ui
-import package_info_plus_macos
+import macos_window_utils
+import package_info_plus
 import path_provider_foundation
-import shared_preferences_macos
+import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppkitUiElementColorsPlugin.register(with: registry.registrar(forPlugin: "AppkitUiElementColorsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
