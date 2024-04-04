2b4178391afc51b3fffd1d0d6378a8d16b915e19
tospery <tospery@gmail.com>
2024-04-05 06:47:21 +0800

    wrap

--- packages/hi_core/lib/src/model/wrap.dart
+++ packages/hi_core/lib/src/model/wrap.dart
@@ -2,12 +2,36 @@ import 'package:flutter/material.dart';
 import 'model.dart';
 import 'package:hi_core/src/datatype.dart';
 import 'package:hi_core/src/logger.dart';
+import 'package:hi_core/src/extension/map.dart';
 
 class HiWrap extends HiModel {
-  final dynamic value;
+  final dynamic data;
 
   const HiWrap({
     super.id,
-    this.value,
+    this.data,
   });
+
+  factory HiWrap.fromJson(Map<String, dynamic> json) => HiWrap(
+    id: json.value<String>('id'),
+    data: json['data'],
+  );
+
+  @override
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'data': data,
+  };
+
+  HiWrap copyWith({
+    String? id,
+    dynamic data,
+  })  => HiWrap(id: id ?? this.id, data: data ?? this.data);
+
+  @override
+  bool get stringify => true;
+
+  @override
+  List<Object?> get props => [id, data];
+
 }
