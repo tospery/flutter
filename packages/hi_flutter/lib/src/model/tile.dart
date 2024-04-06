05e2983eeaef5458b1994c7499fc52f98ef71330
tospery <tospery@gmail.com>
2024-04-06 09:13:47 +0800

    tile

--- packages/hi_flutter/lib/src/model/tile.dart
+++ packages/hi_flutter/lib/src/model/tile.dart
@@ -2,20 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hi_core/hi_core.dart';
 import 'package:hi_flutter/src/widget/tile_cell.dart';
 
-// enum HiTileStyle {
-//   space,
-//   button,
-//   information;
-//
-//   factory HiTileStyle.fromValue(dynamic value) =>
-//     {
-//       HiTileStyle.space.instanceName: HiTileStyle.space,
-//       HiTileStyle.button.instanceName: HiTileStyle.button,
-//       HiTileStyle.information.instanceName: HiTileStyle.information,
-//     }[hiString(value) ?? ''] ??
-//         HiTileStyle.information;
-// }
-
 class HiTile extends HiModel {
   final String? title;
   final String? subTitle;
@@ -23,18 +9,9 @@ class HiTile extends HiModel {
   final String? icon;
   final String? color;
   final double? height;
+  final bool? checked;
   final bool? indicated;
   final bool? separated;
-
-  // HiTileStyle get style {
-  //   if (this.id == 'space') {
-  //     return HiTileStyle.space;
-  //   }
-  //   if (this.id == 'button') {
-  //     return HiTileStyle.button;
-  //   }
-  //   return HiTileStyle.information;
-  // }
 
   Widget? get separator => separated ?? false ? const Divider() : null;
   Widget? cell({HiCellPressed? onPressed, HiValueChanged? onChanged}) => HiTileCell(model: this, onPressed: onPressed,);
@@ -47,20 +24,22 @@ class HiTile extends HiModel {
     this.icon,
     this.color,
     this.height,
+    this.checked,
     this.indicated = true,
     this.separated = true,
   });
 
-  factory HiTile.fromJson(Map<String, dynamic> json) => HiTile(
-        id: json.value<String>('id'),
-        title: json.value<String>('title'),
-        subTitle: json.value<String>('subTitle'),
-        detail: json.value<String>('detail'),
-        icon: json.value<String>('icon'),
-        color: json.value<String>('color'),
-        height: json.value<double>('height'),
-        indicated: json.value<bool>('indicated'),
-        separated: json.value<bool>('separated'),
+  factory HiTile.fromJson(Map<String, dynamic> data) => HiTile(
+        id: hiJson(data)?.value<String>('id'),
+        title: hiJson(data)?.value<String>('title'),
+        subTitle: hiJson(data)?.value<String>('subTitle'),
+        detail: hiJson(data)?.value<String>('detail'),
+        icon: hiJson(data)?.value<String>('icon'),
+        color: hiJson(data)?.value<String>('color'),
+        height: hiJson(data)?.value<double>('height'),
+        checked: hiJson(data)?.value<bool>('checked'),
+        indicated: hiJson(data)?.value<bool>('indicated'),
+        separated: hiJson(data)?.value<bool>('separated'),
       );
 
   @override
@@ -72,6 +51,7 @@ class HiTile extends HiModel {
         'icon': icon,
         'color': color,
         'height': height,
+        'checked': checked,
         'indicated': indicated,
         'separated': separated,
       };
@@ -84,6 +64,7 @@ class HiTile extends HiModel {
     String? icon,
     String? color,
     double? height,
+    bool? checked,
     bool? indicated,
     bool? separated,
   }) {
@@ -95,6 +76,7 @@ class HiTile extends HiModel {
       icon: icon ?? this.icon,
       color: color ?? this.color,
       height: height ?? this.height,
+      checked: checked ?? this.checked,
       indicated: indicated ?? this.indicated,
       separated: separated ?? this.separated,
     );
@@ -109,6 +91,7 @@ class HiTile extends HiModel {
         detail,
         color,
         height,
+        checked,
         indicated,
         separated,
       ];
